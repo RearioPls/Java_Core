d53e638ef1539a6058fc51b848f40629610b817d
dodiiier <darkserness@mail.ru>
2022-05-16 15:10:17 +0300

    Java Core. Примитивные типы. Задача 2.4.13 решение рев.a97347ee

--- primitive/src/primitive2/Main.java
+++ primitive/src/primitive2/Main.java
@@ -3,10 +3,15 @@ package primitive2;
 import java.math.BigInteger;
 
 public class Main {
-        public static void main(String[] args) {
+    public static void main(String[] args) {
+        System.out.println(factorial(3));
+    }
 
+    public static BigInteger factorial(int value) {
+        BigInteger result = BigInteger.ONE;
+        for (int i = 1; i <= value; i++) {
+            result = result.multiply(BigInteger.valueOf(i));
         }
-        public static BigInteger factorial(int value){
-            return null;
-        }
+        return result;
+    }
 }
