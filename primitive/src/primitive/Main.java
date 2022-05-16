f28348fe78aadf5dbe04773aa2eee28d1e4618f4
Pls Nogod <evgeniy.kostin@softline.com>
2022-05-16 09:37:15 +0300

    Java Core. Примитивные типы. Задача 2.1.7

---
+++ primitive/src/primitive/Main.java
@@ -0,0 +1,10 @@
+package primitive;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+    public static double priceCalculation(double price, int count){
+        return 0.0;
+    }
+}
