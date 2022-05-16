f1960c39063d94883540cee25340939b213bf0f7
Pls Nogod <plsnogod@icloud.com>
2022-05-16 10:29:19 +0300

    Java Core. Примитивные типы. Задача 2.2.8

---
+++ primitive/src/primitive5/Main.java
@@ -0,0 +1,11 @@
+package primitive5;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+    public static boolean isPowerOfTwo(int value) {
+        //Твой код здесь
+        return false;
+    }
+}
