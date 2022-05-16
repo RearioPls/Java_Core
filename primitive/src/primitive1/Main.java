6f56c495c86ce9232d34c945a0939f4d4c4e3997
Pls Nogod <plsnogod@icloud.com>
2022-05-16 09:43:24 +0300

    Java Core. Примитивные типы. Задача 2.1.10

---
+++ primitive/src/primitive1/Main.java
@@ -0,0 +1,10 @@
+package primitive1;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+    public static boolean doubleExpression(double a, double b, double c){
+        return false;
+    }
+}
