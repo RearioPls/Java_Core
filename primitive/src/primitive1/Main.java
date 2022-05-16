4b81e2a29ad295c5e0f31a01157de854f3ab99df
dodiiier <darkserness@mail.ru>
2022-05-16 14:49:14 +0300

    Java Core. Примитивные типы. Задача 2.1.10 решение рев.6f56c495

--- primitive/src/primitive1/Main.java
+++ primitive/src/primitive1/Main.java
@@ -2,9 +2,14 @@ package primitive1;
 
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println(doubleExpression(1,1,2));
     }
-    public static boolean doubleExpression(double a, double b, double c){
-        return false;
+
+    public static boolean doubleExpression(double a, double b, double c) {
+        boolean z = false;
+        if (Math.abs(a + b) - c <= 1E-4) {
+            z = true;
+        }
+        return z;
     }
 }
