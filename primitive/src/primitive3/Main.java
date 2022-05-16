845328e93e34007f730d09a87b77200aa73386d4
dodiiier <darkserness@mail.ru>
2022-05-16 15:56:39 +0300

    Java Core. Примитивные типы. Задача 2.1.11 решение рев.4398e0a9

--- primitive/src/primitive3/Main.java
+++ primitive/src/primitive3/Main.java
@@ -2,6 +2,10 @@ package primitive3;
 
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println(drawisMonitorsCounter(100,3));
     }
+    public static int drawisMonitorsCounter(int monitors, int programmers){
+        return monitors%programmers;
+    }
+
 }
