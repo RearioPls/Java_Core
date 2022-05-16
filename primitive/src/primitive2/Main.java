a97347ee352a5de5528b60ea9b075e1458edf5fa
Pls Nogod <plsnogod@icloud.com>
2022-05-16 12:23:38 +0300

    Java Core. Примитивные типы. Задача 2.4.13

---
+++ primitive/src/primitive2/Main.java
@@ -0,0 +1,12 @@
+package primitive2;
+
+import java.math.BigInteger;
+
+public class Main {
+        public static void main(String[] args) {
+
+        }
+        public static BigInteger factorial(int value){
+            return null;
+        }
+}
