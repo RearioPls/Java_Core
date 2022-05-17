abc0e5a2b29eeea06623474485747b03f1ce5262
dodiiier <darkserness@mail.ru>
2022-05-17 11:23:25 +0300

    Java Core. Примитивные типы. Задача 2.3.10 решение рев.56711ac8

--- primitive/src/primitive6/Main.java
+++ primitive/src/primitive6/Main.java
@@ -2,6 +2,11 @@ package primitive6;
 
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println(isPalindrome("Was it a cat I saw?"));
+    }
+    public static boolean isPalindrome(String text){
+        String str = text.replaceAll("[^a-zA-Z0-9]", "");
+        StringBuilder sb = new StringBuilder(str);
+        return str.equalsIgnoreCase(sb.reverse().toString());
     }
 }
