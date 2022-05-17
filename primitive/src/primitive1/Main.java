034f872eaff7214181995d9f200b0e4ccc193005
dodiiier <darkserness@mail.ru>
2022-05-17 10:39:04 +0300

    Java Core. Примитивные типы. Задача 2.1.10 решение рев.6f56c495 исправленно рев.c949ee82

--- primitive/src/primitive1/Main.java
+++ primitive/src/primitive1/Main.java
@@ -1,17 +1,11 @@
 package primitive1;
-/*
-Исправить, отрефакторить метод doubleExpression, убрать лишние переменные и if
- */
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println(doubleExpression(1,1,2));
-    } // давно 1 или 2 стали double?
+        System.out.println(doubleExpression(1.1, 1.1, 2.7));
+    }
 
-    public static boolean doubleExpression(double a, double b, double c) {  // записать ретурн в одну строку без лишних переменных
-        boolean z = false;
-        if (Math.abs(a + b) - c <= 1E-4) {
-            z = true;
-        }
-        return z;
+    public static boolean doubleExpression(double a, double b, double c) {
+        return (Math.abs(a + b - c) <= 1E-4);
     }
 }
