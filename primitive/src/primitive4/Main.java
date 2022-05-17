0540de0ea88ac814ea276ac9b63c7dcba281d317
dodiiier <darkserness@mail.ru>
2022-05-17 10:56:04 +0300

    Java Core. Примитивные типы. Задача 2.2.2 решение рев.11c2464a исправленно рев.23cf972e

--- primitive/src/primitive4/Main.java
+++ primitive/src/primitive4/Main.java
@@ -1,12 +1,10 @@
 package primitive4;
-/*
-В условии сказано сделать со слэшом, переделай
- */
+
 public class Main {
     public static void main(String[] args) {
         System.out.println(charExpression(0));
     }
     public static char charExpression(int a){
-return (char)(92+a);
+return (char)('\\'+a);
     }
 }
