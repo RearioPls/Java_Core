462983dab8a536b8e778813549ed744cd0bedb63
dodiiier <darkserness@mail.ru>
2022-05-18 11:42:10 +0300

    Java Core. Примитивные типы. Задача 2.4.16 решение рев.0d3976c2

--- primitive/src/primitive8/Main.java
+++ primitive/src/primitive8/Main.java
@@ -1,7 +1,20 @@
 package primitive8;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
+        int[] arr = new int[]{3, 5, 20, 8, 7, 3, 100};
+        printOddNumbers(arr);
+    }
 
+    public static void printOddNumbers(int[] arr) {
+        ArrayList<String> list = new ArrayList<>();
+        for (int i : arr)
+            if ((i & 1) == 1)
+                list.add(i + "");
+        System.out.print(String.join(", ", list));
+        System.out.println();
     }
 }
+
