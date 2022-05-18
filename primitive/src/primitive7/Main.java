e5cddace47c0e8d76c91fbe9d71276669dc218e7
dodiiier <darkserness@mail.ru>
2022-05-18 10:45:52 +0300

    Java Core. Примитивные типы. Задача 2.4.15 решение рев.3beeef18

--- primitive/src/primitive7/Main.java
+++ primitive/src/primitive7/Main.java
@@ -2,6 +2,21 @@ package primitive7;
 
 public class Main {
     public static void main(String[] args) {
+        //Main volleyball = new Main(); - это для проверки создан объект.
+        //System.out.println(volleyball.determineGroup(11));
 
     }
-}
+
+    public int determineGroup(int age) {
+       if(6<age && age<14) {
+           return 1;
+       }
+       if(13<age && age<18){
+           return 2;
+       }
+        if(17<age && age<66) {
+            return 3;
+        }
+        return -1;
+    }
+    }
