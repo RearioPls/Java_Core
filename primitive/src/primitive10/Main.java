0fac8eb3b1aed3cbb62a0827e74b71619ade7145
dodiiier <darkserness@mail.ru>
2022-05-18 15:25:36 +0300

    Java Core. Примитивные типы. Задача 2.4.18 решение рев.c02fe52a

--- primitive/src/primitive10/Main.java
+++ primitive/src/primitive10/Main.java
@@ -2,13 +2,14 @@ package primitive10;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(printTextPerRole(roles,textLines));;
+        System.out.println(printTextPerRole(roles, textLines));
     }
-    static String [] roles= {
-            "Городничий","Аммос Федорович",
+
+    static String[] roles = {
+            "Городничий", "Аммос Федорович",
             "Артемий Филиппович",
             "Лука Лукич"};
-    static String [] textLines={
+    static String[] textLines = {
             "Городничий: Я пригласил вас, господа, с тем, чтобы сообщить вам пренеприятное известие: к нам едет ревизор.",
             "Аммос Федорович: Как ревизор?",
             "Артемий Филиппович: Как ревизор?",
@@ -16,9 +17,20 @@ public class Main {
             "Аммос Федорович: Вот те на!",
             "Артемий Филиппович: Вот не было заботы, так подай!",
             "Лука Лукич: Господи боже! еще и с секретным предписаньем!"};
+
     public static String printTextPerRole(String[] roles, String[] textLines) {
+        StringBuilder sb = new StringBuilder();
+        for (String role : roles) {
+            sb.append(role + "\n");
 
+            for (int i = 0; i < textLines.length; i++) {
+                if (textLines[i].startsWith(role + ":")) {
+                    sb.append((i + 1) + ")" + textLines[i].substring(role.length() + 1) + "\n");
+                }
 
-        return null;
+            }
+            sb.append("\n");
+        }
+            return sb.toString();
+        }
     }
-}
